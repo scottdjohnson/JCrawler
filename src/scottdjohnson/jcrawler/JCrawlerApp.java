20808a7bd4dcfee5ebf9dfe0df29be1cf7098e06
Scott Johnson <sjohnson@ip-172-31-18-191.us-west-2.compute.internal>
2013-09-17 18:53:28 +0000

    Use as simply a main file, refactor class so it can be accessed by the servlet

--- src/scottdjohnson/jcrawler/JCrawlerApp.java
+++ src/scottdjohnson/jcrawler/JCrawlerApp.java
@@ -12,6 +12,7 @@ import java.io.IOException;
 
 import scottdjohnson.binarytree.URLNode;
 import scottdjohnson.binarytree.BinaryTree;
+import scottdjohnson.jcrawler.JCrawler;
 
 public class JCrawlerApp 
 {	
@@ -26,57 +27,6 @@ public class JCrawlerApp
 		bt.insertNode(un);
 		un.save();
 		
-		getLinksFromURL(bt, url, un.getKey());
+		JCrawler.getLinksFromURL(bt, url, un.getKey());
 	}
-	
-	private static void getLinksFromURL( BinaryTree bt, String url, long parent)
-	{		
-		try{
-			Document doc = Jsoup.connect(url).get();
-			Elements links = doc.select("a[href]");
-			int size		= links.size();
-		
-			// First we store all the URLs at the current level
-			for (int i = 0; i < size; i++)
-			{
-				Element link = links.get(i);
-				
-				// We are going to assume that an absolute URL points to an external
-				//	external web site. Not an entirely safe assumption but ok for this demo
-				
-				URLNode un = new URLNode( link.attr("abs:href"), parent);
-				
-				if ( !bt.isNodeInTree( un ) )
-				{
-					bt.insertNode( un );
-					un.save();
-					System.out.println("Inserting URL: " + link.attr("href"));
-				}
-			}				
-			
-			// We now go through the links again and look recursively, except for links we have already looked at
-			for (int i = 0; i < size; i++)
-			{
-				Element link 	= links.get(i);
-				URLNode un 	= new URLNode( link.attr("abs:href"), parent);
-				URI u           = new URI(link.attr("href"));
-
-				if ( !bt.isNodeInTree( un ) )
-					if (!u.isAbsolute())
-						getLinksFromURL( bt, un.getUrl(), un.getKey() );
-
-			}
-		}
-		catch(URISyntaxException e)
-		{
-			System.out.println("This is not a valid URL");
-		}
-		catch(IOException e)
-		{
-			System.out.println("Could not connect to this URL");
-
-		}
-
-	}
-
 }
