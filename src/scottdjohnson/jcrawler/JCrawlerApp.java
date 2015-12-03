5ff68f1c87d39564b5279071f505289c24209372
Scott Johnson <sjohnson@ip-172-31-18-191.us-west-2.compute.internal>
2015-12-03 23:33:54 +0000

    Use new functions

--- src/scottdjohnson/jcrawler/JCrawlerApp.java
+++ src/scottdjohnson/jcrawler/JCrawlerApp.java
@@ -9,6 +9,8 @@ import org.jsoup.select.Elements;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.OutputStream;
 
 import scottdjohnson.binarytree.URLNode;
 import scottdjohnson.binarytree.BinaryTree;
@@ -29,7 +31,7 @@ import java.util.List;
  *
  * @author Scott Johnson
  **/
-public class JCrawlerApp 
+public class JCrawlerApp
 {	
 	/**
 	 * Main function for launching JCrawler
@@ -37,53 +39,15 @@ public class JCrawlerApp
 	 */
 	public static void main(String[] args)
 	{
-		System.out.println(args.length);
-
 		if (args.length > 0)
 		{
-			BinaryTree bt = new BinaryTree();
-			URLNode un = new URLNode(args[0]);
-			DBConnector.open();
-
-			Validate.isTrue(args.length == 1, "usage: supply url to fetch");
-			String url 	= args[0];
-			
-			bt.insertNode(un);
-			DBConnector.save(un);
-			
-			JCrawler.getLinksFromURL(bt, url, un.getKey());
-			DBConnector.close();
+			JCrawler.addUrl(args[0]);
 		}
 		else
 		{
-
-			List list;
-			String urlkey = "0";//request.getParameter("urlkey");
-			//PrintWriter out = response.getWriter();
-
-			DBConnector.open();
-
-
-			// If there is a specifc key, get it, otherwise get all of them
-	                if (null != urlkey)
-        	        	list = DBConnector.getFromQuery("from URLNode url_list where parent_key = " + urlkey);
-			else
-				list = DBConnector.getFromQuery("from URLNode url_list where parent_key = 0");
-
-			// Loop through all the results from the query
-			for (int i = 0; i < list.size(); i++)
-			{
-				// Count the total number of results that have this URL as a parent
-				int count =(int) ((DBConnector.getFromQuery("select count(*) from URLNode where parent_key="
-					+ ((URLNode)list.get(i)).getKey())).get(0));
-
-				// Print out this URL with the number of its children
-				System.out.println("<a href='' onclick=\"return getAJAX(" + Integer.toString((int)((URLNode)list.get(i)).getKey() ) + ");\">" 
-					+ ((URLNode)list.get(i)).getUrl() + "</a> (" + Integer.toString(count) + ")<br />");
-			}
-
-
-			DBConnector.close();
+			JCrawler.getUrls(0, new PrintWriter((OutputStream)System.out));
+		System.out.println("next");
+			JCrawler.deleteAllUrls();
 		}
 
 	}
